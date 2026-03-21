0e8b3774d54411572f5952988ef056966f94259f
Manu199 <Manu199gh@gmail.com>
2026-03-21 23:15:36 +0100

    [completed] main page

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const cur=document.getElementById('cursor');
+const ring=document.getElementById('cursor-ring');
+let mx=0,my=0,rx=0,ry=0;
+document.addEventListener('mousemove',e=>{
+  mx=e.clientX;my=e.clientY;
+  cur.style.left=mx+'px';cur.style.top=my+'px';
+});
+
+(function tick(){
+  rx+=(mx-rx)*0.12;ry+=(my-ry)*0.12;
+  ring.style.left=rx+'px';ring.style.top=ry+'px';
+  requestAnimationFrame(tick);
+})();
